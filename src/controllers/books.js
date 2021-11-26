181a362eb564d74a20678015f11b8e425f6175ef
pierluigi <pierluigibaiano@gmail.com>
2021-11-26 14:12:50 +0100

    deep population to have an author of each comment

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,15 +1,19 @@
-import BooksModel from '../models/books.js';
+import BooksModel from "../models/books.js";
 
 export const getAllBooks = async () => {
-	const books = await BooksModel.find({})
-		.populate("author")
-		.populate("customers")
-		.populate([
-			{
-				path: "comments",
-				model: "Comment",
-				select: "author message datetime"
-			}
-		]);
-	return books;
-}
+  const books = await BooksModel.find({})
+    .populate("author")
+    .populate("customers")
+    .populate([
+      {
+        path: "comments",
+        model: "Comment",
+        select: "author message datetime",
+        populate: {
+          path: "author",
+          model: "Person",
+        },
+      },
+    ]);
+  return books;
+};
